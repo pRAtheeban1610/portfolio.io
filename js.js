d7ce33042a59d0981f5624d0f89750b0ee45e186
pRAtheeban <109447739+pRAtheeban1610@users.noreply.github.com>
2022-08-21 06:31:47 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,39 @@
+document.getElementById("whatsapp").addEventListener("click",()=>{
+    window.open("https://wa.me/919791388916")
+})
+document.getElementById("whatsapp11").addEventListener("click",()=>{
+    window.open("https://wa.me/919791388916")
+})
+document.getElementById("gmail").addEventListener("click",()=>{
+    window.open("https://mail.google.com/mail/u/0/#inbox")
+})
+document.getElementById("gmail11").addEventListener("click",()=>{
+    window.open("https://mail.google.com/mail/u/0/#inbox")
+})
+document.getElementById("github").addEventListener("click",()=>{
+    window.open("https://github.com/pRAtheeban1610")
+})
+
+document.getElementById("linkedin").addEventListener("click",()=>{
+    window.open("https://www.linkedin.com/in/pratheeban-nainar-30b32115a/")
+})
+    document.getElementById("linkedin11").addEventListener("click",()=>{
+        window.open("https://www.linkedin.com/in/pratheeban-nainar-30b32115a/")
+        })
+document.getElementById("portfolio").addEventListener("click",()=>{
+    window.open("https://github.com/pRAtheeban1610/portfolio.io")
+})
+document.getElementById("random-quote-generator").addEventListener("click",()=>{
+    window.open("https://pratheeban1610.github.io/RandomQuoteGenerator.io/")
+})
+document.getElementById("bmi-calculator").addEventListener("click",()=>{
+    window.open("https://pratheeban1610.github.io/BMICalculator.io/")
+})
+
+document.getElementById("contact-me").addEventListener("click",()=>{
+    document.getElementById("contact-home").style.display = "block"
+    document.getElementById("close-btn").addEventListener("click",()=>{
+        document.getElementById("contact-home").style.display = "none"  
+    })
+})
+
